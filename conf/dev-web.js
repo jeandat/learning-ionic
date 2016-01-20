e9b1e00b4a81fa09615721e6e10546a9b0925bbb
jdat82 <jean.dat@gmail.com>
2016-01-20 21:22:23 +0100

    Unused file.

--- conf/dev-web.js
+++
@@ -1,7 +0,0 @@
-var pkg = require('../package.json');
-var extend = require('node.extend');
-module.exports = extend(true, {}, require('./dev'), {
-    ngCordovaScript: 'js/ng-cordova-mocks.js',
-    ngCordovaModuleName: 'ngCordovaMocks',
-    apiEndpoint: '/api'
-});
